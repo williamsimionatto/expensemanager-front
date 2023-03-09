8d32eba2dd14765fccf1058a4765858db091f779
William Simionatto <william.sn0226@gmail.com>
2023-03-09 19:51:43 -0300

    feat: create ListPeriods usecase

---
+++ src/domain/model/period-model.ts
@@ -0,0 +1,11 @@
+export type Period = {
+  id: number
+  name: string
+  startDate: Date
+  endDate: Date
+  budget: number
+  reservedBudget: number
+  remainingBudget: number
+  usedBudget: number
+  remainingUsedBudget: number
+}
