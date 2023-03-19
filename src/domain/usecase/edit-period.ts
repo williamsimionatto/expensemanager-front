38b29e93ae37612865813e14e793aa8d2800d9b6
William Simionatto <william.sn0226@gmail.com>
2023-03-19 18:42:12 -0300

    feat: add EditPeriod usecase

---
+++ src/domain/usecase/edit-period.ts
@@ -0,0 +1,27 @@
+import { RemoteCategoryResultModel, RemotePeriodResultModel } from "../model"
+
+export interface EditPeriod {
+  edit: (id: string, params: EditPeriod.Params) => Promise<EditPeriod.Result>
+}
+
+export namespace EditPeriod {
+  export type Params = {
+    name: string
+    startDate: string
+    endDate: string
+    budget: number
+    categories: RemoteAddPeriodCategory[]
+  }
+
+  export type Result = RemotePeriodResultModel
+  export type Error =  {
+    details: string;
+    message: string;
+    timestamp: Date;
+  }
+
+  export type RemoteAddPeriodCategory = {
+    category: RemoteCategoryResultModel
+    budget: number
+  }
+}
